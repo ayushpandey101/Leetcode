930406597c780b4e65fd2cbb11f44fdc408b951e
Codegeass <127859041+ayushpandey101@users.noreply.github.com>
2025-06-19 19:33:55 +0530

    Time: 7 ms (98.58%) | Memory: 59.7 MB (10.64%) - LeetSync

--- 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.java
+++ 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.java
@@ -1,33 +1,32 @@
 class Solution {
     public int partitionArray(int[] nums, int k) {
-        Arrays.sort(nums);
         int largest = Integer.MIN_VALUE;
 
-        for(int i = 0; i<nums.length; i++){
-            largest = Math.max(largest,nums[i]);
+        for (int i = 0; i < nums.length; i++) {
+            largest = Math.max(largest, nums[i]);
         }
 
-        int arr[] = new int[largest+1];
-        for(int i =0; i<nums.length; i++){
+        int arr[] = new int[largest + 1];
+        for (int i = 0; i < nums.length; i++) {
             arr[nums[i]]++;
         }
 
         int min = -1;
         int cnt = 0;
-        for(int i = 0; i <= largest; i++){
-            if(arr[i]>0){
-                if(min == -1){
+        for (int i = 0; i <= largest; i++) {
+            if (arr[i] > 0) {
+                if (min == -1) {
                     min = i;
                 }
-                while(arr[i]>0){
-                    if(i - min >k){
-                        cnt ++;
+                while (arr[i] > 0) {
+                    if (i - min > k) {
+                        cnt++;
                         min = i;
                     }
                     arr[i]--;
                 }
             }
         }
-        return cnt+1;
+        return cnt + 1;
     }
 }
