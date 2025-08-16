05cffae860c0db05e15010291922216a8516706c
Codegeass <127859041+ayushpandey101@users.noreply.github.com>
2025-08-16 21:27:23 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (72.77%) - LeetSync

---
+++ 1448-maximum-69-number/maximum-69-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maximum69Number (int num) {
+       char[] str= String.valueOf(num).toCharArray();
+       for(int i=0;i<str.length;i++){
+        if(str[i]=='6'){
+            str[i]='9';
+            break;
+        }
+       }
+       return Integer.parseInt(new String(str));
+    }
+}
