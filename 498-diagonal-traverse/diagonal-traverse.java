e436426f5902d68a575eb2ce39068748ebaf58a5
Codegeass <127859041+ayushpandey101@users.noreply.github.com>
2025-08-25 19:17:45 +0530

    Time: 2 ms (99.41%) | Memory: 47.1 MB (70.40%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.java
+++ 498-diagonal-traverse/diagonal-traverse.java
@@ -1,37 +1,25 @@
-class Solution {
-  public int[] findDiagonalOrder(int[][] matrix) {
-    final int m = matrix.length;
-    final int n = matrix[0].length;
-    int[] ans = new int[m * n];
-    int d = 1; // left-bottom -> right-top
-    int row = 0;
-    int col = 0;
+public class Solution {
+    public int[] findDiagonalOrder(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) return new int[0];
 
-    for (int i = 0; i < m * n; ++i) {
-      ans[i] = matrix[row][col];
-      row -= d;
-      col += d;
-      // out-of-bounds
-      if (row == m) {
-        row = m - 1;
-        col += 2;
-        d = -d;
-      }
-      if (col == n) {
-        col = n - 1;
-        row += 2;
-        d = -d;
-      }
-      if (row < 0) {
-        row = 0;
-        d = -d;
-      }
-      if (col < 0) {
-        col = 0;
-        d = -d;
-      }
+        int m = matrix.length, n = matrix[0].length;
+        int[] result = new int[m * n];
+        int row = 0, col = 0;
+
+        for (int i = 0; i < m * n; i++) {
+            result[i] = matrix[row][col];
+
+            if ((row + col) % 2 == 0) {
+                if (col == n - 1) row++;
+                else if (row == 0) col++;
+                else { row--; col++; }
+            } else {
+                if (row == m - 1) col++;
+                else if (col == 0) row++;
+                else { row++; col--; }
+            }
+        }
+
+        return result;
     }
-
-    return ans;
-  }
 }
