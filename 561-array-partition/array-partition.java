59019fd38c7febd07e0910cac8f15d388524cdcd
Codegeass <127859041+ayushpandey101@users.noreply.github.com>
2025-09-01 21:24:56 +0530

    Time: 5 ms (99.68%) | Memory: 46.8 MB (34.24%) - LeetSync

--- 561-array-partition/array-partition.java
+++ 561-array-partition/array-partition.java
@@ -1,11 +1,20 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
-        Arrays.sort(nums);
-        int ans=0;
-        for(int i=0;i<nums.length;i+=2){
-            
-            ans+=Math.min(nums[i],nums[i+1]);
-        }
-        return ans;
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+            for (int num: nums) {
+                if(min > num) min = num;
+                if(max < num) max = num;
+            }
+            int[] freq = new int[max - min + 1];
+            for (int num: nums) {
+                freq[num - min]++;
+            }
+            int sum = 0, rem = 1;
+            for (int i = 0; i < freq.length; i++) {
+                int num = freq[i];  
+                sum += ((num + rem) >> 1) * (i + min);
+                rem ^= num & 1;
+            }
+        return sum;
     }
 }
