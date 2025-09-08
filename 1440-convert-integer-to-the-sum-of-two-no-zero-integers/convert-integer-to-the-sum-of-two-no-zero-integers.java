97e891604d36c3429cf6c2497e65abda2e984e71
Codegeass <127859041+ayushpandey101@users.noreply.github.com>
2025-09-08 17:51:30 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (58.05%) - LeetSync

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.java
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.java
@@ -1,18 +1,23 @@
 class Solution {
-    
-    private boolean hasZero(int num) {
-        return String.valueOf(num).contains("0");
-    }
-
     public int[] getNoZeroIntegers(int n) {
-
-        for (int i = 1; i < n; i++) {
-            int j = n - i;
-
-            if (!hasZero(i) && !hasZero(j)) {
-                return new int[]{i, j};
+      
+  
+       for(int i=1;i<=n;i++){
+            int a = i;
+            int b = n-i;
+            if(a>0 && b>0 && iscontainsZero(a) == false && iscontainsZero(b) == false){
+                return new int[]{a,b};
             }
+       }
+    
+    return new int[] {-1,-1};
+    }
+    public boolean iscontainsZero(int n){
+        while(n > 0){
+            int digits = n % 10;
+            if(digits == 0) return true;
+            n /= 10;
         }
-        return new int[]{}; 
+        return false;
     }
 }
